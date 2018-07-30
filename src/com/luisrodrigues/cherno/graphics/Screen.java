beed4a2920646e2c6dae9f9139a275724cd4a80e
Luís Rodrigues <luis.cnm@gmail.com>
2018-07-30 18:33:19 +0100

    Added GrassTile class
    Working on the renderTile method inside the Screen class

--- src/com/luisrodrigues/cherno/graphics/Screen.java
+++ src/com/luisrodrigues/cherno/graphics/Screen.java
@@ -2,6 +2,8 @@ package com.luisrodrigues.cherno.graphics;
 
 import java.util.Random;
 
+import com.luisrodrigues.cherno.level.tile.Tile;
+
 public class Screen {
 	
 	private int width, height;
@@ -51,5 +53,16 @@ public class Screen {
 			}
 		}
 	}
+	
+	
+	public void renderTile(int xp, int yp, Tile tile) {
+		
+		for(int y = 0; y < tile.sprite.SIZE; y++) {
+			int ya = y + yp;
+			
+		}
+		
+	}	
+	
 	
 }
---
+++ src/com/luisrodrigues/cherno/level/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package com.luisrodrigues.cherno.level.tile;
+
+import com.luisrodrigues.cherno.graphics.Screen;
+import com.luisrodrigues.cherno.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		//Do render stuff here
+	}
+
+}
